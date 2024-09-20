91828fc793f7bdf97f65352a76e5311dfc7f913c
pauldub04 <paul.dubnyakov@gmail.com>
2024-09-20 21:57:55 +0300

    add computing

--- server/src/compute.c
+++ server/src/compute.c
@@ -1,5 +1,10 @@
 #include "compute.h"
 
 void compute(int rows, int cols, int** matrix, int* vector, int* result) {
-    // TODO: add computing
+    for (int i = 0; i < rows; ++i) {
+        result[i] = 0;
+        for (int j = 0; j < cols; ++j) {
+            result[i] += matrix[i][j] * vector[j];
+        }
+    }
 }
