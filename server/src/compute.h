3f768edbb6c2034444258a9ebe3ff049d32ba587
pauldub04 <paul.dubnyakov@gmail.com>
2024-10-12 09:34:11 +0300

    add openmp to server

--- server/src/compute.h
+++ server/src/compute.h
@@ -1,6 +1,10 @@
 #ifndef COMPUTE_H
 #define COMPUTE_H
 
+#include <omp.h>
+
+#define THREAD_NUM 2
+
 void compute(int rows, int cols, int** matrix, int* vector, int* result);
 
 #endif /* COMPUTE_H */
